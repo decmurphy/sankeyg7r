a3750b1d2aa2c5470ad948ee53eec9a966e27ef4
Declan Murphy <declan.murphy@flightclub.io>
2023-04-11 18:07:53 +0300

    refine categories

--- src/main/kotlin/xyz/decmurphy/sankeyg7r/Categoriser.kt
+++ src/main/kotlin/xyz/decmurphy/sankeyg7r/Categoriser.kt
@@ -7,35 +7,7 @@ import xyz.decmurphy.sankeyg7r.config.AppConfig
 class Categoriser(
     val appConfig: AppConfig
 ) {
-
     fun process(entry: String, isIncome: Boolean): AppCategory? {
-
         return appConfig.categories.find { entry belongsTo it }
-// //
-//        if (entry.startsWith("To ")) return Category.TRANSFERS_OUT
-//        if (entry.startsWith("365 Online")) return Category.TRANSFERS_OUT
-//        if (entry.equals("Revolut Bank UAB") || entry.equals("Revolut Payments UAB")) {
-//            return if (isIncome) Category.TRANSFERS_IN else Category.TRANSFERS_OUT
-//        }
-//
-//        if (entry.contains("BOI CR CARD")) return Category.FC_EXPENSES
-//        if (entry.contains("Google Gsuite_flightcl")) return Category.FC_EXPENSES
-//        if (entry.contains("Google*gsuite Flightcl")) return Category.FC_EXPENSES
-//        if (entry.contains("digitalocean.com")) return Category.FC_EXPENSES
-//        if (entry.contains("Name-Cheap.com")) return Category.FC_EXPENSES
-//
-//        if (entry.contains("Leap Card")) return Category.TRANSPORT
-//
-//        if (entry.contains("Grooming Room")) return Category.MISC
-//        if (entry.contains("ticketmaster")) return Category.MISC
-//        if (entry.contains("Boots")) return Category.MISC
-//        if (entry.contains("Cash at")) return Category.MISC
-//        if (entry.contains("zalando")) return Category.MISC
-//        if (entry.contains("Boots")) return Category.MISC
-//        if (entry.contains("Boots")) return Category.MISC
-//
-//        if (entry.contains("OXFAM")) return Category.CHARITY
-//
-//        return if (isIncome) Category.UNCLASSIFIED_INCOME else Category.UNCLASSIFIED_OUTGOING
     }
 }
