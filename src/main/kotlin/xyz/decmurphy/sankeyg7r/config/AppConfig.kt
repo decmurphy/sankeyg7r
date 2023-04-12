a68b98dcdd9609c784f6de8719f48b10f2c6b7eb
Declan Murphy <declan.murphy@flightclub.io>
2023-04-12 23:52:17 +0300

    fix import

--- src/main/kotlin/xyz/decmurphy/sankeyg7r/config/AppConfig.kt
+++ src/main/kotlin/xyz/decmurphy/sankeyg7r/config/AppConfig.kt
@@ -3,7 +3,7 @@ package xyz.decmurphy.sankeyg7r.config
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
-import xyz.decmurphy.sankeyg7r.Category
+import xyz.decmurphy.sankeyg7r.categories.Category
 
 @ConstructorBinding
 @ConfigurationProperties(prefix = "app")
